e5a57460a2f6049be410f1568598b349d0efacc5
Matt Faucher <51377027+mattfaucher@users.noreply.github.com>
2023-02-07 18:10:47 -0500

    Setup sending http requests, added some abstractions for creating and receiving bytes from socket

--- client.py
+++ client.py
@@ -1,19 +1,24 @@
+#!/usr/bin/env python3
 # CLI for runninng the webcrawler.
 
 import argparse
+import socket_connection
 
 from html_parse import parse_all_href, parse_all_secret_flags
+from socket_connection import login_to_server
 
 
 def main(args):
     username = args.username
     password = args.password
-    pass
+
+    login_to_server(username, password, 443)
+    return
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='Fakebook Webcrawler')
-    parser.add_argument('username', type=str, dest='username')
-    parser.add_argument('password', type=str, dest='password')
+    parser.add_argument('username', type=str)
+    parser.add_argument('password', type=str)
     args = parser.parse_args()
     main(args)
