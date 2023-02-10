9a94fa32bd805419625b501ca1e6970c2cfb0d0d
Matt Faucher <51377027+mattfaucher@users.noreply.github.com>
2023-02-10 17:34:13 -0500

    Updating sessionid each time a new service is called, rough implementation of client, flags outputting, not sure if right

--- client.py
+++ client.py
@@ -2,7 +2,6 @@
 # CLI for runninng the webcrawler.
 
 import argparse
-import socket_connection
 
 from html_parse import parse_all_href, parse_all_secret_flags
 from socket_connection import HTTPSocket
@@ -15,6 +14,25 @@ def main(args):
         username=args.username,
         password=args.password,
     )
+
+    headers = {
+        'Host': client.host,
+        'Cookie': f'sessionid={client.session_id}; csrftoken={client.csrftoken}',
+    }
+
+    flags = []
+    queue = []
+    visited = set()
+    queue.append('/fakebook/')
+    while queue and len(flags) < 5:
+        response = client.make_get_request(path=queue.pop(0), headers=headers, connection_alive=True)
+        for href in parse_all_href(response):
+            if href not in visited:
+                queue.append(href)
+            visited.add(href)
+        flags.extend(parse_all_secret_flags(response))
+        print(flags)
+    client.sock.close()
 
 
 if __name__ == '__main__':
